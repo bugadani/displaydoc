66aeceb1c3acb40f316ac339fb791c7cfc724fff
Dániel Buga <bugadani@gmail.com>
2024-02-27 15:48:27 +0100

    Hammer on tests

--- tests/compile_tests.rs
+++ tests/compile_tests.rs
@@ -1,7 +1,8 @@
 #[allow(unused_attributes)]
+#[cfg(feature = "std")]
 #[rustversion::attr(not(nightly), ignore)]
 #[test]
-fn no_std() {
+fn compile_test_std() {
     let t = trybuild::TestCases::new();
 
     t.pass("tests/no_std/enum_prefix.rs");
@@ -10,15 +11,23 @@ fn no_std() {
     t.pass("tests/no_std/multiple.rs");
     t.pass("tests/no_std/with.rs");
 
-    #[cfg(not(feature = "std"))]
-    {
-        t.compile_fail("tests/no_std/without.rs");
-        t.compile_fail("tests/no_std/enum_prefix_missing.rs");
-    }
+    t.compile_fail("tests/std/without.rs");
+    t.compile_fail("tests/std/enum_prefix_missing.rs");
+}
 
-    #[cfg(feature = "std")]
-    {
-        t.compile_fail("tests/std/without.rs");
-        t.compile_fail("tests/std/enum_prefix_missing.rs");
-    }
+#[allow(unused_attributes)]
+#[cfg(not(feature = "std"))]
+#[rustversion::attr(not(nightly), ignore)]
+#[test]
+fn compile_test_no_std() {
+    let t = trybuild::TestCases::new();
+
+    t.pass("tests/no_std/enum_prefix.rs");
+    t.pass("tests/no_std/multi_line_allow.rs");
+    t.pass("tests/no_std/multi_line.rs");
+    t.pass("tests/no_std/multiple.rs");
+    t.pass("tests/no_std/with.rs");
+
+    t.compile_fail("tests/no_std/without.rs");
+    t.compile_fail("tests/no_std/enum_prefix_missing.rs");
 }
